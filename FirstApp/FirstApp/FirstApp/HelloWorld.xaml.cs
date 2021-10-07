49ad76c349c9e7523a82eec5431ed4d3a94c5c6b
kadir1278 <kadirdestek1@gmail.com>
2021-10-07 20:32:45 +0300

    Login & HelloWorld

---
+++ FirstApp/FirstApp/FirstApp/HelloWorld.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FirstApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HelloWorld : ContentPage
+    {
+        public HelloWorld()
+        {
+            InitializeComponent();
+            Device.StartTimer(new TimeSpan(), () =>
+            {
+                date.Text = "Tarih : " + DateTime.Now.ToString("dd.MM.yyyy");
+                time.Text = "Saat : " + DateTime.Now.ToString("HH.mm.ss");
+                return true;
+            });
+            Device.StartTimer(new TimeSpan(0,0,5), () =>
+            {
+                loginModal.Children.Clear();
+                return false;
+            });
+        }
+    }
+}
---
+++ FirstApp/FirstApp/FirstApp/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FirstApp
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+            Device.StartTimer(new TimeSpan(), () =>
+            {
+                date.Text = "Tarih : " + DateTime.Now.ToString("dd.MM.yyyy");
+                time.Text = "Saat : " + DateTime.Now.ToString("HH.mm.ss");
+                return true;
+            });
+        }
+
+        private async void loginUser(object sender, EventArgs e)
+        {
+            if (username.Text == "admin" && password.Text == "1234")
+            {
+                await DisplayAlert("Success", "", "Ok");
+                await Navigation.PushModalAsync(new HelloWorld());
+            }
+            else
+            {
+                await DisplayAlert("Warning", "Wrong username or password", "Ok");
+                username.Text = ("");
+                password.Text = ("");
+            }
+        }
+
+        private async void clearEntry(object sender, EventArgs e)
+        {
+            username.Text = ("");
+            password.Text = ("");
+        }
+
+    }
+}
